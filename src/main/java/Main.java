9bdd9705ce5483c13026d82a125a3c33cb9c892e
Natalia <natalia.molla222@gmail.com>
2022-09-21 12:55:25 -0700

    test

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -4,10 +4,8 @@ public class Main {
     //comment left by jacob
     //comment again by matt
 
-<<<<<<< HEAD
-    //Testing forced push
 
+    //Testing forced push + after the mumbo jumbo was deleted
 
     //pushed again
->>>>>>> 7c5da66e632553d0b074cf52d0adf0e89cd0ca68
 }
