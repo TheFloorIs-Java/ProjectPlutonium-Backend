9231d55d26e8f103f3f3b3989cca08137a2d731b
laalsanedo <rpatel1320@yahoo.com>
2022-09-21 14:59:58 -0500

    updates

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -3,11 +3,4 @@ public class Main {
     //Comment by nat v2
     //comment left by jacob
     //comment again by matt
-
-<<<<<<< HEAD
-    //Testing forced push
-
-
-    //pushed again
->>>>>>> 7c5da66e632553d0b074cf52d0adf0e89cd0ca68
 }
