1269741071cf2402ed4d26eb7fe63a9d4ce5cd70
MattHill92 <matthew765@revature.net>
2022-09-21 18:11:44 -0700

    Added Connection utility and Gradle dependencies

---
+++ src/main/java/Utility/ConnectionUtil.java
@@ -0,0 +1,22 @@
+package Utility;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class ConnectionUtil {
+    private static Connection conn;
+    public static Connection getConnection(){
+        if (conn == null)
+            try{
+                String url = "jdbc:sqlserver://projecttwoserver.database.windows.net:1433;database=p2db;encrypt=true;trustServerCertificate=false;hostNameInCertificate=*.database.windows.net;loginTimeout=30;";
+                String username = System.getenv("jacob367@projecttwoserver");
+                String password = System.getenv("plut0nium");
+
+                conn = DriverManager.getConnection(url,username,password);
+            }catch(SQLException e){
+                e.printStackTrace();
+            }
+        return conn;
+    }
+}
