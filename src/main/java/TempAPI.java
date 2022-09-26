eac5425386b98fa348ce5fe7ac82f1bafaa1019c
MattHill92 <matthew765@revature.net>
2022-09-25 18:36:04 -0700

    Completed Test Endpoints

--- src/main/java/TempAPI.java
+++ src/main/java/TempAPI.java
@@ -1,10 +1,34 @@
-import Models.User;
+import Models.*;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import io.javalin.Javalin;
 
 
 public class TempAPI {
 
+    static User[] users = {
+            new User(1, "Matt", "*****", 1, "*****", null, null),
+            new User(2, "Raj", "*****", 1, "*****", null, null),
+            new User(3, "Natalia", "*****", 1, "*****", null, null),
+            new User(4, "Jacob", "*****", 1, "*****", null, null),
+            new User(5, "Corey", "*****", 1, "*****", null, null)
+    };
+    static PublishedGame publishedGame = new PublishedGame(1, "Test Game Title", "Type", "[{'description':'Seek shelter','map':'../../../assets/images/map.png','actions':[{'actionLabel':'search the pizzeria','index':1},{'actionLabel':'search the neaby appartment','index':2},{'actionLabel':'search the mall','index':3}]},{'description':'You are in the pizzeria','map':'../../../assets/images/map.png','actions':[{'actionLabel':'check the oven','index':2},{'actionLabel':'check the perimeter','index':3},{'actionLabel':'step back outside','index':0}]},{'description':'Test 1','map':'../../../assets/images/map.png','actions':[{'actionLabel':'search the pizzaria','index':1},{'actionLabel':'search the neaby appartment','index':2},{'actionLabel':'search the mall','index':3}]},{'description':'Seek shelter','map':'../../../assets/images/map.png','actions':[{'actionLabel':'search the pizzaria','index':1},{'actionLabel':'search the neaby appartment','index':2},{'actionLabel':'search the mall','index':3}]}]", users[2], 6);
+
+    static ScoreCard[] scoreCards = {
+            new ScoreCard(users[0], publishedGame, null, 5),
+            new ScoreCard(users[1], publishedGame, null, 10),
+            new ScoreCard(users[2], publishedGame, null, 8),
+            new ScoreCard(users[3], publishedGame, null, 4),
+            new ScoreCard(users[4], publishedGame, null, 20)
+    };
+
+    static HighScoreCard[] highScoreCards = {
+            new HighScoreCard(users[0], 5),
+            new HighScoreCard(users[1], 10),
+            new HighScoreCard(users[2], 8),
+            new HighScoreCard(users[3], 4),
+            new HighScoreCard(users[4], 20)
+    };
 
     public static void main(String[] args) {
 
@@ -25,31 +49,59 @@ public class TempAPI {
 
         app.get("/", ctx -> ctx.result("Hello World!"));
 
-        //post login/ ( supply username and password and receive User object )
+
+
+
+        //get users/id
+        app.get("/users/id/{id}", ctx -> ctx.json(users[Integer.parseInt(ctx.pathParam("id"))-1]));
+        app.get("/users/session", ctx -> {
+           String sid = ctx.header("session");
+           if (sid != null && sid != "") ctx.json(users[0]);
+           else ctx.status(401);
+        });
+        app.get("/users/all", ctx -> ctx.json(users));
+
+
         app.post("/login", ctx -> {
             ObjectMapper mapper = new ObjectMapper();
             User requestUser = mapper.readValue(ctx.body(), User.class);
-            ctx.result("Test");
+            requestUser.setSessionId("ThisWouldBeASessionID");
+            ctx.json(requestUser);
         });
-
-        //get user/id
-        app.get("/users/id/{id}", ctx -> ctx.json(new User()));
-
-        app.get("/users/all", ctx -> ctx.json(new User[]{new User(), new User(), new User()}));
-
-        //get user/username
-        app.get("/users/username/{name}", ctx -> ctx.json(new User()));
-
-        //post register/ ( supply User object and Service will add it to database )
-        app.post("/register", ctx -> {
+        app.post("/users", ctx -> {
             ObjectMapper mapper = new ObjectMapper();
             User requestUser = mapper.readValue(ctx.body(), User.class);
-            ctx.result("Test");
+            requestUser.setSessionId("ThisWouldBeASessionID");
+            ctx.json(requestUser);
+        });
+
+
+        app.get("/publishedGame/id/{id}", ctx -> ctx.json(publishedGame));
+        app.get("/publishedGame/userId/{id}", ctx -> ctx.json(new PublishedGame[] {publishedGame, publishedGame}));
+        app.get("/publishedGame/date/{date}", ctx -> ctx.json(publishedGame));
+
+
+        app.get("/scoreCards/userId/{id}", ctx -> ctx.json(new ScoreCard[] {scoreCards[Integer.parseInt(ctx.pathParam("id"))], scoreCards[Integer.parseInt(ctx.pathParam("id"))], scoreCards[Integer.parseInt(ctx.pathParam("id"))]}));
+        app.get("/highScoreCards/all", ctx -> ctx.json(highScoreCards));
+
+
+        app.get("/echo", ctx -> ctx.result(ctx.body()));
+        app.post("/echo", ctx -> ctx.result(ctx.body()));
+
+
+
+
+
+
+        // Depreciated===================================================================================
+        app.post("/publishedGame", ctx -> {
+            ObjectMapper mapper = new ObjectMapper();
+            PublishedGame requestGame = mapper.readValue(ctx.body(), PublishedGame.class);
+            ctx.json(requestGame);
         });
 
         app.get("/hangman/date/{date}", ctx -> ctx.result("this is the test game. try your best!"));
 
-        app.get("/echo", ctx -> ctx.result(ctx.body()));
-        app.post("/echo", ctx -> ctx.result(ctx.body()));
+
     }
 }
