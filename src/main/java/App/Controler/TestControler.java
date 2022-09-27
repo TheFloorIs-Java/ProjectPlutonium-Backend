25e5905593afea756d44d8b998092493a3253278
MattHill92 <matthew765@revature.net>
2022-09-27 07:27:32 -0700

    moved packages

--- src/main/java/App/Controler/TestControler.java
+++ src/main/java/App/Controler/TestControler.java
@@ -1,4 +1,4 @@
-package Controler;
+package App.Controler;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class TestControler {
 
-    @GetMapping()
+    @GetMapping("/")
     public String hello(){
         return "Hello API!";
     }
