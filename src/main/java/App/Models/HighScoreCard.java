650a01845848bdabc6ba7bab20b6a3dd1319431a
MattHill92 <matthew765@revature.net>
2022-09-28 13:04:29 -0700

    Fixing TempAPI and Lombok'd HighScoreCard

--- src/main/java/App/Models/HighScoreCard.java
+++ src/main/java/App/Models/HighScoreCard.java
@@ -1,32 +1,16 @@
 package App.Models;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Entity;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
 public class HighScoreCard {
     private User user;
     private int score;
-
-    public HighScoreCard() {
-        this.user = new User();
-        this.score = -1;
-    }
-
-    public HighScoreCard(User user, int score) {
-        this.user = user;
-        this.score = score;
-    }
-
-    public User getUser() {
-        return user;
-    }
-
-    public void setUser(User user) {
-        this.user = user;
-    }
-
-    public int getScore() {
-        return score;
-    }
-
-    public void setScore(int score) {
-        this.score = score;
-    }
 }
