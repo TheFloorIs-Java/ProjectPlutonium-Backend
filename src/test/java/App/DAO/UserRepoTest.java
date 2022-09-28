1cf35e58ab759d3fbe9ee68b3201bb087f731fee
laalsanedo <rpatel1320@yahoo.com>
2022-09-28 17:15:47 -0500

    added user repo tests

--- src/test/java/App/DAO/UserRepoTest.java
+++ src/test/java/App/DAO/UserRepoTest.java
@@ -2,6 +2,7 @@ package App.DAO;
 
 import App.App;
 import App.Controller.UserController;
+import App.Models.User;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
@@ -11,27 +12,48 @@ import org.springframework.context.annotation.Bean;
 
 import static org.junit.jupiter.api.Assertions.*;
 
-@SpringBootTest(classes = App.class)
+@SpringBootTest
 class UserRepoTest {
 
 
 
         @Autowired
-        public UserController uc;
-        @Autowired
         public UserRepository ur;
 
 
         @Test
-        public void testGetAllUsers()
+        public void addUser()
         {
-            assertTrue(this.uc.getAllUsers().size() != 0);
+            User user = User.builder()
+                    .username("laalsanedo")
+                    .password("2525")
+                    .permission_level(0)
+                    .salt("esfbsdfvsdf")
+                    .build();
+
+            ur.save(user);
         }
 
         @Test
-        public void getUsernameById()
-        {
-            assertEquals("Matt", ur.getReferenceById(1).getUsername());
+        public void deleteUser(){
+            User user = ur.findById(7).get();
+
+            ur.delete(user);
+
+        }
+
+        @Test
+    public void getUserByID(){
+            User user = ur.findById(3).get();
+            System.out.println(user.getUsername());
+        }
+
+        @Test
+        public void updateUserPermissionLevel(){
+            User existingUser = ur.findById(2).get();
+            existingUser.setPermission_level(0);
+            ur.save(existingUser);
+
         }
 
 
