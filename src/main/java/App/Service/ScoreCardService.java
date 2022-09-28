2320569e40fc682c8a5d627e0724bc6d7931c960
Natalia <natalia.molla222@gmail.com>
2022-09-28 12:26:31 -0700

    worked on score card controller and service

--- src/main/java/App/Service/ScoreCardService.java
+++ src/main/java/App/Service/ScoreCardService.java
@@ -1,4 +1,28 @@
 package App.Service;
 
+
+import App.DAO.ScoreCardRepo;
+import App.Models.ScoreCard;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+@Component
 public class ScoreCardService {
+
+    ScoreCardRepo scr;
+    @Autowired
+    public ScoreCardService(ScoreCardRepo scr) {
+        this.scr = scr;
+    }
+
+    public ScoreCard getScoreCardByPlayerId(){
+        return null;
+    }
+
+    public ScoreCard addScoreByPlayerId() {
+        return null;
+    }
+
+    public void addScoreCard(ScoreCard scoreCard) {
+    }
 }
