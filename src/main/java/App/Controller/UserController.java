96bca6332854b522fc51da4cde66baa310a341d7
MattHill92 <matthew765@revature.net>
2022-09-28 11:39:35 -0700

    userControler work

--- src/main/java/App/Controller/UserController.java
+++ src/main/java/App/Controller/UserController.java
@@ -1,4 +1,32 @@
 package App.Controller;
 
+import App.Models.*;
+import App.Service.SpringTestService;
+import App.Service.UserServiceNew;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class UserController {
+
+    private final SpringTestService sts;
+    private final UserServiceNew us;
+
+    @Autowired
+    public UserController(SpringTestService sts, UserServiceNew us){
+        this.sts = sts;
+        this.us = us;
+    }
+
+
+    @GetMapping("/users/id/{id}")
+    public User getUserById(@PathVariable("id") int id){
+
+        User user = us.getUserInfo(id);
+
+        return new User();
+    }
 }
