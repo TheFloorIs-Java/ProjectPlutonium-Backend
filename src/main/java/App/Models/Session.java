984f493c0ea90719b56262db61c6b617de24f551
MattHill92 <matthew765@revature.net>
2022-09-28 12:38:49 -0700

    added session model

---
+++ src/main/java/App/Models/Session.java
@@ -0,0 +1,20 @@
+package App.Models;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Entity;
+import java.util.Date;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@EqualsAndHashCode
+@Entity
+public class Session {
+    User user;
+    String session_id;
+    Date session_expiry;
+}
