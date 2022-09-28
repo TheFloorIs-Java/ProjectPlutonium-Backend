383d7b5eca214f7663527f9243c68178aac05a3b
laalsanedo <rpatel1320@yahoo.com>
2022-09-28 16:59:20 -0500

    user change

--- src/main/java/App/Models/User.java
+++ src/main/java/App/Models/User.java
@@ -1,9 +1,6 @@
 package App.Models;
 
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.EqualsAndHashCode;
-import lombok.NoArgsConstructor;
+import lombok.*;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
@@ -15,6 +12,7 @@ import java.util.Date;
 @NoArgsConstructor
 @Entity
 @Table(name="user_info")
+@Builder
 public class User {
 
     @Id
