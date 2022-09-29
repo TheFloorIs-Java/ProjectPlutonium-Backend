c041ff710dce215aac48111295cc8be3c670955f
Natalia <natalia.molla222@gmail.com>
2022-09-29 13:55:22 -0700

    added enable auto config in app

--- src/main/java/App/App.java
+++ src/main/java/App/App.java
@@ -2,9 +2,13 @@ package App;
 
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.context.annotation.Configuration;
 
-
+@Configuration
+@EnableAutoConfiguration(exclude={DataSourceAutoConfiguration.class})
 @SpringBootApplication
 public class App {
 
