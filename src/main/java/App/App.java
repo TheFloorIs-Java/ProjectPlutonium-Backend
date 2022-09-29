9157498163adf42ba0a4e24966433831eb236125
Natalia <natalia.molla222@gmail.com>
2022-09-29 15:47:32 -0700

    trying diff approach with the config

--- src/main/java/App/App.java
+++ src/main/java/App/App.java
@@ -6,9 +6,7 @@ import org.springframework.boot.autoconfigure.AutoConfiguration;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
-import org.springframework.boot.jdbc.DataSourceBuilder;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
+
 
 import javax.sql.DataSource;
 
