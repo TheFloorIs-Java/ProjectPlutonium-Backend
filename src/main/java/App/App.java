4adae073618714acff092104941535cf192355e2
Natalia <natalia.molla222@gmail.com>
2022-09-29 14:15:55 -0700

    trying a diff way to initialize data source

--- src/main/java/App/App.java
+++ src/main/java/App/App.java
@@ -5,14 +5,34 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.boot.jdbc.DataSourceBuilder;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
+import javax.sql.DataSource;
 
 
 @SpringBootApplication
 public class App {
 
+    @Configuration
+    public class DatasourceConfig {
+        @Bean
+        public DataSource datasource() {
+            return DataSourceBuilder.create()
+                    .driverClassName("com.microsoft.sqlserver.jdbc.SQLServerDriver")
+                    .url("jdbc:sqlserver://projecttwoserver.database.windows.net:1433;database=p2db;encrypt=true;trustServerCertificate=false;hostNameInCertificate=*.database.windows.net;loginTimeout=30")
+                    .username("jacob367")
+                    .password("Plut0nium")
+                    .build();
+
+        }
+    }
+
     public static void main(String[] args) {
         SpringApplication.run(App.class);
     }
+
+
 
 }
